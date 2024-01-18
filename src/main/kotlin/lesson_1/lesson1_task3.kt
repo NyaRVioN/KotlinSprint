fb5a79fd56e07f41fce799b3d98428ad57db3b52
NaRVioN <marlinegorov@yandex.com>
2024-01-18 19:03:20 +0700

    KS-1-3 create task3

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    println("Год полёта: $year")
+    println("Час взлёта: $hour")
+    println("Минута взлёта: $minute")
+
+    hour = 10
+    minute = 55
+    println("Время посадки: $hour:$minute")
+
+    //println("Время посадки: " + hour + ":" + minute)
+    //Второй вариант
+
+}
+
