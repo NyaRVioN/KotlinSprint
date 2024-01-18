d43d0f788296b031e61eb65aae5d130138abc83c
NaRVioN <marlinegorov@yandex.com>
2024-01-18 18:27:48 +0700

    KS-1-1 create task2

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankYouText: String = "Спасибо за вашу покупку!"
+    println("Количество заказов: $numberOfOrders")
+    println(thankYouText)
+
+    //val numberOfWorkers: Int = 2000
+    //println("Количество работников: $numberOfWorkers")
+
+    val numberOfWorkers: Int = 1999
+    println("Количество работников: $numberOfWorkers")
+}
