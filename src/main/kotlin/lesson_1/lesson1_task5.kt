aa15c4f7661b7953f73b29aeba94609243ef9beb
NaRVioN <marlinegorov@yandex.com>
2024-01-19 20:09:18 +0700

    KS-1-5 fix task5

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,16 @@
 package org.example.lesson_1
+
 fun main() {
+
+    val secondsInMinutes = 60
+    val minutesInHour = 60
+
     val seconds = 6480
+    val minutes = seconds / secondsInMinutes
+    val hours = minutes / minutesInHour
 
-    val minutes = seconds /60
-    val hours = minutes /60
-
-    val remainingSeconds = seconds % 60
-    val remainingMinutes = minutes % 60
+    val remainingSeconds = seconds % secondsInMinutes
+    val remainingMinutes = minutes % minutesInHour
 
     val formattedTime = String.format("%02d:%02d:%02d", hours, remainingMinutes, remainingSeconds)
     println(formattedTime)
