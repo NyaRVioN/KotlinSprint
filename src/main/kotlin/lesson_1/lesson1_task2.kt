3dbf33cbd1c3b54cc6dd7094b64075d2fb42d36c
NaRVioN <marlinegorov@yandex.com>
2024-01-19 16:06:59 +0700

    KS-2-2 do task2

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -6,9 +6,9 @@ fun main() {
     println("Количество заказов: $numberOfOrders")
     println(thankYouText)
 
-    //val numberOfWorkers: Int = 2000
+    var numberOfWorkers: Int = 2000
     //println("Количество работников: $numberOfWorkers")
 
-    val numberOfWorkers: Int = 1999
+    numberOfWorkers = 1999
     println("Количество работников: $numberOfWorkers")
 }
